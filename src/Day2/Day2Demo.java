deaad2eb60c4546e87a892e9e295b7393bfd965b
pepep <pepep@Matte>
2023-04-07 22:39:41 -0400

    initial commit

---
+++ src/Day2/Day2Demo.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class Day2Demo {
+	
+	public static void main (String [] args) {
+		
+		System.out.println("Day 2 Git");
+	
+
+	System.out.println("ADD line of code");
+}
+}
